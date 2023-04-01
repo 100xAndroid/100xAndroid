f86b09d062b8827146e9577e0abca306cd7b0bc0
100xAndroid <128327670+100xAndroid@users.noreply.github.com>
2023-04-01 19:00:18 +0200

    Prvni verze zdrojovych souboru

---
+++ 10_Ladeni_aplikaci_logovani/LogCat/app/src/main/java/cz/programujemeproandroid/logcat/MainActivity.java
@@ -0,0 +1,47 @@
+package cz.programujemeproandroid.logcat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void log1 (View sender) {
+        // logovani urovne DEBUG
+        Log.d ("MainActivity", "Test log DEBUG");
+    }
+
+    public void log2 (View sender) {
+        // logovani urovne ERROR
+        Log.e ("MainActivity", "Test log ERROR");
+    }
+
+    public void log3 (View sender) {
+        // logovani urovne INFO
+        Log.i ("MainActivity", "Test log INFO");
+    }
+
+    public void stackTrace (View sender) {
+        try {
+            // zavolame metodu, ktera vyvola vyjimku
+            vyvolatVyjimku();
+        }
+        catch (Exception ex) {
+            // vypiseme stacktrace
+            ex.printStackTrace();
+        }
+    }
+
+    // Metoda umele zpusobi chybu
+    private void vyvolatVyjimku () throws Exception {
+        // vyvolame vyjimku
+        throw new Exception("Testovací výjimka");
+    }
+}
