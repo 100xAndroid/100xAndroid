492e8ed9a5bdfd9cd3237dc746024b5c3320263f
100xAndroid <128327670+100xAndroid@users.noreply.github.com>
2024-05-12 23:39:26 +0200

    Update GetData.java
    Zastaraly AsyncTask predelany na ExecutorService

--- 20_Pripojeni_k_internetu/Internet1/app/src/main/java/cz/stokratandroid/internet1/GetData.java
+++ 20_Pripojeni_k_internetu/Internet1/app/src/main/java/cz/stokratandroid/internet1/GetData.java
@@ -1,49 +1,57 @@
 package cz.stokratandroid.internet1;
 
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.net.ssl.HttpsURLConnection;
 
-public class GetData extends AsyncTask<Void, Void, Void> {
-
+public class GetData {
     String nactenaData = "";
 
-    // operace ktera bude spustena na pozadi
-    @Override
-    protected Void doInBackground(Void... voids) {
-        try
-        {
-            URL url= new URL("https://jsonkeeper.com/b/XFIM");
-            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-            InputStream stream = conn.getInputStream();
-            BufferedReader buff = new BufferedReader(new InputStreamReader(stream));
+    // operace, ktera bude spustena na pozadi
+    protected Void startAsync() {
 
-            String radka = "";
-            do {
-                nactenaData = nactenaData + radka;
-                radka = buff.readLine();
-            } while (radka != null);
-        }
-        catch(Exception e) {
-            // MainActivity.jsonTextView.setText(e.getMessage());
-            nactenaData = "Chyba: " + e.getMessage();
-            return null;
-        }
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    URL url = new URL("https://www.jsonkeeper.com/b/XFIM");
+                    HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+                    InputStream stream = conn.getInputStream();
+                    BufferedReader buff = new BufferedReader(new InputStreamReader(stream));
+
+                    String radka = "";
+                    do {
+                        nactenaData = nactenaData + radka;
+                        radka = buff.readLine();
+                    } while (radka != null);
+                } catch (Exception e) {
+                    // MainActivity.jsonTextView.setText(e.getMessage());
+                    nactenaData = "Chyba: " + e.getMessage();
+                }
+
+                handler.post(new Runnable() {
+                    // udalost volana po skonceni operace
+                    @Override
+                    public void run() {
+                        MainActivity.jsonTextView.setText(nactenaData);
+                    }
+                });
+            }
+        });
 
         return null;
     }
-
-    // metoda se zavola po skonceni operace
-    @Override
-    protected void onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
-
-        MainActivity.jsonTextView.setText(nactenaData);
-    }
-
 }
-
